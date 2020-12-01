905950f26617f292adc5a22529ecf381256fada2
rin@kingkit101.com <>
2020-12-01 14:57:31 +0800

    12.1 2

---
+++ home-ball.js
@@ -0,0 +1,26 @@
+function homeBall(){
+    $(window).scroll(function(){
+        var scrollVal = $(this).scrollTop()
+        var headerH = $('header').height()
+        var bannerH = $('.banner-block').height()
+
+
+        if(scrollVal>){
+            // $('.back-to-top-float').fadeIn(300)
+        }else{
+            
+        }
+    })
+}
+
+
+
+
+
+var windowWidth = $(window).width()
+
+if(widdowWidth>992){
+    homeBall()
+}
+
+
--- navi.js
+++ navi.js
@@ -1,15 +1,17 @@
 $('.hamburger-icon').click(function(){
     $('.hamburger-icon').fadeOut(300)
     $('.close-icon').fadeIn(800)
-    $('.navi').animate({top:'0'},500)
-    $('.navi .nav').fadeIn(1200)
+    $('.navi').animate({top:'0'},300)
+    $('.navi .nav').fadeIn(800)
+    $('.navi .fb-link').fadeIn(600)
     $('html,body').addClass('back-fixed')
 })
 
 $('.close-icon').click(function(){
     $('.close-icon').fadeOut(300)
     $('.hamburger-icon').fadeIn(800)
-    $('.navi').animate({top:'-100vh'},500)
+    $('.navi').animate({top:'-100vh'},300)
     $('.navi .nav').fadeOut(100)
+    $('.navi .fb-link').fadeOut(300)
     $('html,body').removeClass('back-fixed')
 })
