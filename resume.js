b14e26b6ff55880c2b532c5d60fb1f0ba862383e
rin@kingkit101.com <>
2020-12-23 17:18:18 +0800

    12.23 resume

--- resume.js
+++ resume.js
@@ -1,21 +1,26 @@
-//list
+$(document).ready(function(){
 
-var listBarHeight = $('.list-bar').height()
-var listFrameHeight = photoContainerHeight + 45
+    //list
 
-$('.inside-list-open').click(function(){
-    $('.co-list .list-frame').animate({height:listBarHeight},800)
-    $('.inside-list-btn').fadeOut()
+    $('.inside-list-open').click(function(){
+
+        var listBarHeight = $('.list-bar').height()
+
+        $('.co-list .list-frame').animate({height:listBarHeight},800)
+        $('.inside-list-btn').fadeOut()
+
+    })
+
+    //photo
+
+    $('.photo-btn').click(function(){
+
+        var photoContainerHeight = $('.photo-container').height()
+        var photoFrameHeight = photoContainerHeight + 45
+
+        $('.photo-frame').animate({height:photoFrameHeight},800)
+        $('.photo-btn').fadeOut()
+    })
+
 })
 
-
-
-//photo
-
-var photoContainerHeight = $('.photo-container').height()
-var photoFrameHeight = photoContainerHeight + 45
-
-$('.photo-btn').click(function(){
-    $('.photo-frame').animate({height:photoFrameHeight},800)
-    $('.photo-btn').fadeOut()
-})
